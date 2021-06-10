3bf60e2d6119a42083fe3e3bdf786590cfd460f1
thirty2 <84319334+thirty2@users.noreply.github.com>
2021-06-10 17:28:34 +0530

    Update less-comfortable.c

--- pset1/mario/less-comfortable.c
+++ pset1/mario/less-comfortable.c
@@ -3,26 +3,26 @@
 
 int main(void)
 {
-    int pyramid;//input from user
+    int pyramid;
     do
     {
-        pyramid = get_int("Enter a number between 0 and 9: \n");
+        pyramid = get_int("Enter a number between 0 and 9: \n");//taking the input
     }
-    while (pyramid < 1 || pyramid > 8);
-    int i;
+    while (pyramid < 1 || pyramid > 8);//making sure that the number entered is between 1 and 8
+    int i;//declaring i
     for (i = 0; pyramid > i; i++)
     {
         for (int j = 0; pyramid > j; j++)
         {
             if (i + j < pyramid - 1)
             {
-                printf(" ");
+                printf(" ");//printing the spaces
             }
             else
             {
-                printf("#");
+                printf("#");//printing the hashes
             }
         }
-        printf("\n");
+        printf("\n");//printing a new line
     }
 }
